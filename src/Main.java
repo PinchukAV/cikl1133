d696290bf34ffcb59ff56876e71ea3061e642af6
PinchukAV <angel71.05@mail.ru>
2022-11-03 20:33:39 +0300

    для вас

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int [] = new int [3]:
-        int [] weight = "(1.57, 1.654, 9.986)
+        int [] weight = "(1.57, 1.654, 9.986):
+                123 jht
     }
 }
