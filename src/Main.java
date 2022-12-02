d3bceacd9463e45862192cd5817bea2cd8aa74b1
PinchukAV <angel71.05@mail.ru>
2022-12-02 20:31:54 +0300

    для вас

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] weingh = new int[3];
+        int[] weingh = {1,2,3};
         weingh[0] = 1;
         for (int i = 0; i < weingh.length; i++) {
             System.out.println(weingh[i]);
